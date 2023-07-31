dc8e126885b6e7a80188d85c3487e66afab60480
Loïc Bourgois <loic@bourgois.pro>
2023-07-31 14:27:39 +0200

    up

--- commands/deploy_remote.sh
+++ commands/deploy_remote.sh
@@ -10,7 +10,4 @@ git status
 sleep 1
 # docker run hello-world
 $HOME/github.com/loicbourgois/downtowhat/commands/build_data.sh
-rm /home/gravitle/github.com/loicbourgois/downtowhat_local/data/cast_member_label.raw
-rm /home/gravitle/github.com/loicbourgois/downtowhat_local/data/film_cast_member_2.raw
-rm /home/gravitle/github.com/loicbourgois/downtowhat_local/data/film_cast_member_4.raw
 $HOME/github.com/loicbourgois/downtowhat/commands/tail_raw.sh
