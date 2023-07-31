5ceb77842004c3bbf26156a51ecda0e09c7cc4da
Loïc Bourgois <loic@bourgois.pro>
2023-07-31 14:43:29 +0200

    up

--- commands/build_data.sh
+++ commands/build_data.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-mkdir -p $HOME/github.com/loicbourgois/downtowhat_local/data/map
+set -e
+mkdir -p $HOME/github.com/loicbourgois/downtowhat_local/data/map/
 docker compose \
   --file $HOME/github.com/loicbourgois/downtowhat/data_builder/docker-compose.yml \
   up \
