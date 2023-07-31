e4c272e847e49c9528451e75ce689a040c1a5480
Loïc Bourgois <loic@bourgois.pro>
2023-07-31 13:52:46 +0200

    up

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 set -e
 path=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".path")
+path_root=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".path_root")
 user=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".user")
 host=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".host")
 # ssh -i $path $user@$host
+# ssh -i $path_root root@$host
+# ssh -i $HOME/.ssh/hetzner-1633820@loic-spate root@136.243.64.165
+# cat $HOME/.ssh/loic@mac-perso@hetzner
 cat $HOME/github.com/loicbourgois/downtowhat/commands/deploy_remote.sh | ssh -i $path $user@$host 'bash -s'
--- commands/deploy_remote.sh
+++ commands/deploy_remote.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -e
+mkdir -p $HOME/github.com/loicbourgois
+cd $HOME/github.com/loicbourgois
+git clone https://github.com/loicbourgois/downtowhat.git || true
+cd $HOME/github.com/loicbourgois/downtowhat
+git fetch --all
+git checkout origin/movie
 ls
