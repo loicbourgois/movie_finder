57b00ca4a258facbddb98472f96b7739e593b41d
Loïc Bourgois <loic@bourgois.pro>
2023-07-31 14:44:55 +0200

    up

--- commands/build_data.sh
+++ commands/build_data.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
-mkdir -p $HOME/github.com/loicbourgois/downtowhat_local/data/map/
-ls $HOME/github.com/loicbourgois/downtowhat_local/data
-# docker compose \
-#   --file $HOME/github.com/loicbourgois/downtowhat/data_builder/docker-compose.yml \
-#   up \
-#   --renew-anon-volumes --build --force-recreate --remove-orphans
+docker compose \
+  --file $HOME/github.com/loicbourgois/downtowhat/data_builder/docker-compose.yml \
+  up \
+  --renew-anon-volumes --build --force-recreate --remove-orphans
--- data_builder/_main.sh
+++ data_builder/_main.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 cd /root/github.com/loicbourgois
+mkdir -p /root/github.com/loicbourgois/downtowhat_local/data/map/
 python -m downtowhat.data_builder.main
