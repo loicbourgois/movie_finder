bba2becbc22423c9e6248c2284a9c4805be7990d
Loïc Bourgois <loic@bourgois.pro>
2023-07-31 15:03:22 +0200

    up

---
+++ commands/admin_root.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+set -e
+path_root=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".path_root")
+host=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".host")
+ssh -i $path_root root@$host
