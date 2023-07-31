5f6678d5324ec9e7cf6c3cbe38700a1acc387251
Loïc Bourgois <loic@bourgois.pro>
2023-07-31 17:04:56 +0200

    up

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -8,4 +8,4 @@ path=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r "
 path_root=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".path_root")
 user=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".user")
 host=$(cat $HOME/github.com/loicbourgois/downtowhat_local/secrets.json | jq -r ".host")
-cat $HOME/github.com/loicbourgois/downtowhat/commands/deploy_remote.sh | ssh -i $path $user@$host 'bash -s'
+# cat $HOME/github.com/loicbourgois/downtowhat/commands/deploy_remote.sh | ssh -i $path $user@$host 'bash -s'
