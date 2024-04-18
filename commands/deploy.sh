0f3f46089a2bb1106614bbf983ea2f33d2a762e4
Loïc Bourgois <loic@bourgois.pro>
2024-04-18 12:15:33 +0200

    up

--- commands/deploy.sh
+++ commands/deploy.sh
@@ -8,5 +8,5 @@ path=$(cat $HOME/github.com/loicbourgois/movie_finder_local/secrets.json | jq -r
 path_root=$(cat $HOME/github.com/loicbourgois/movie_finder_local/secrets.json | jq -r ".path_root")
 user=$(cat $HOME/github.com/loicbourgois/movie_finder_local/secrets.json | jq -r ".user")
 host=$(cat $HOME/github.com/loicbourgois/movie_finder_local/secrets.json | jq -r ".host")
-echo "ssh -i $path $user@$host"
-# cat $HOME/github.com/loicbourgois/movie_finder/commands/deploy_remote.sh | ssh -i $path $user@$host 'bash -s'
+# echo "ssh -i $path $user@$host"
+cat $HOME/github.com/loicbourgois/movie_finder/commands/deploy_remote.sh | ssh -i $path $user@$host 'bash -s'
